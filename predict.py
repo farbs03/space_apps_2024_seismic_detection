6fdfe489437c344ab0ed14a0a3abd995808d1389
kambhani <63814718+kambhani@users.noreply.github.com>
2024-10-06 16:49:51 -0400

    Add test3.py

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 from eventdetector_ts.prediction.prediction import predict
 from eventdetector_ts.prediction.utils import plot_prediction
 import pandas as pd
+import hashlib
 
 data = pd.read_csv('./data/lunar/training/data/S12_GradeA/xa.s12.00.mhz.1970-01-19HR00_evid00002.csv')
 inputs = data[["time_rel(sec)", "velocity(m/s)"]]
